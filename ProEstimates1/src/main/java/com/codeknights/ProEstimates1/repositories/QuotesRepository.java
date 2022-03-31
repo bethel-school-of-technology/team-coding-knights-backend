aaf5da73c93c5b5a090b5408d25c46ba4ae96dc0
Brian DeWitt <80775722+slugguts@users.noreply.github.com>
2022-03-30 20:18:43 -0700

    Debug
    Commenting out everything besides the User Repo/Controller

--- ProEstimates1/src/main/java/com/codeknights/ProEstimates1/repositories/QuotesRepository.java
+++ ProEstimates1/src/main/java/com/codeknights/ProEstimates1/repositories/QuotesRepository.java
@@ -1,4 +1,4 @@
-package com.codeknights.ProEstimates1.repositories;
+/*package com.codeknights.ProEstimates1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -6,4 +6,4 @@ import com.codeknights.ProEstimates1.models.Quotes;
 
 public interface QuotesRepository extends JpaRepository<Quotes, Integer>{
 
-}
+}*/
--- ProEstimates1/src/main/java/com/codeknights/ProEstimates1/repositories/UserRepository.java
+++ ProEstimates1/src/main/java/com/codeknights/ProEstimates1/repositories/UserRepository.java
@@ -1,9 +1,16 @@
 package com.codeknights.ProEstimates1.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.codeknights.ProEstimates1.models.User;
 
-public interface UserRepository extends JpaRepository<User,String>{
-	User findByUsername(String user_email);
-}
+@Repository
+public interface UserRepository extends JpaRepository <User, String>{
+
+	User findByUsername(String user_email);}
+
+
+//Need to put our thingys in here so that they do the thingy and create something useable by the service.
