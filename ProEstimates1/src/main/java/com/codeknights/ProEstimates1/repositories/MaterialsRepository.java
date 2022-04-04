054fdebb949206790aa1c75a1d7bf947a222f909
calebmbyrne <87716084+calebmbyrne@users.noreply.github.com>
2022-04-03 19:46:13 -0500

    Delete ProEstimates1 directory

--- ProEstimates1/src/main/java/com/codeknights/ProEstimates1/repositories/MaterialsRepository.java
+++
@@ -1,9 +0,0 @@
-/*package com.codeknights.ProEstimates1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.codeknights.ProEstimates1.models.Materials;
-
-public interface MaterialsRepository extends JpaRepository<Materials, Integer>{
-
-}*/
