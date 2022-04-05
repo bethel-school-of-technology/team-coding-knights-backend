33c7e3b34d4424aca2e6b6a4f4911188a96bf5dd
Brian DeWitt <80775722+slugguts@users.noreply.github.com>
2022-04-04 19:10:35 -0700

    updated for POST

--- ProEstimates1/src/main/java/com/codeknights/ProEstimates1/repositories/UserRepository.java
+++ ProEstimates1/src/main/java/com/codeknights/ProEstimates1/repositories/UserRepository.java
@@ -1,14 +1,12 @@
 package com.codeknights.ProEstimates1.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.codeknights.ProEstimates1.models.User;
 
 @Repository
-public interface UserRepository extends JpaRepository< User, String> {
-
-//	User findByuser_email(String user_email);
-
-	//public User findByUsername(String user_email);
-}
+public interface UserRepository extends JpaRepository <User, String>{
+	User findByUsername(String user_email);}
