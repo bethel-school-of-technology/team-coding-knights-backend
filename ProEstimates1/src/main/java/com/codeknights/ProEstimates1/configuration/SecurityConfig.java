2ae684b5eff9ff091f8249745919d0c958b2fd1f
Brian DeWitt <80775722+slugguts@users.noreply.github.com>
2022-04-09 12:24:09 -0700

    spring security setup
    setting up security features

--- ProEstimates1/src/main/java/com/codeknights/ProEstimates1/configuration/SecurityConfig.java
+++ ProEstimates1/src/main/java/com/codeknights/ProEstimates1/configuration/SecurityConfig.java
@@ -1,65 +1,29 @@
 package com.codeknights.ProEstimates1.configuration;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.codeknights.ProEstimates1.repositories.QuotesRepository;
-import com.codeknights.ProEstimates1.services.MySQLUserDetailsService;
-
-
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	
-	private MySQLUserDetailsService mySQLUserDetailsService;
-	
-	private QuotesRepository quotesRepository;
-	
-	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-	
 
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-         .userDetailsService(mySQLUserDetailsService)
-         .passwordEncoder(passwordEncoder());
-    }
-  @Override
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-//				
-				.anyRequest().permitAll();
-		http
-		.cors().and().csrf().disable();
+				.antMatchers("/css/**", "/index").permitAll()		
+				.antMatchers("/user/**").hasRole("USER")			
+				.and()
+			.formLogin()
+				.loginPage("/login").failureUrl("/login-error");	
 	}
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http
-//			.authorizeRequests()
-////				This is where we will probably have to add the Angular Front End work
-////				once we know what it's called and what kind of file is use.  Just like
-////				the JavaScript file "/js/**.js" below.
-//				.antMatchers("/**","/", "/user", "/register","/login", "/contractors", "/js/**.js").permitAll()
-//				.anyRequest().permitAll()
-//				.and()
-//			.formLogin()
-//				.loginPage("/login")
-//				.permitAll()
-//				.and()
-//			.logout()
-//				.permitAll();
-//	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password("password").roles("USER");
+	}
 }
-//				.anyRequest().authenticated()
--- ProEstimates1/src/main/java/com/codeknights/ProEstimates1/configuration/WebSecurity.java
+++ ProEstimates1/src/main/java/com/codeknights/ProEstimates1/configuration/WebSecurity.java
@@ -1,33 +1,13 @@
-package com.codeknights.ProEstimates1.configuration;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/css/**", "/index").permitAll()		
-				.antMatchers("/user/**").hasRole("USER")			
-				.and()
-			.formLogin()
-				.loginPage("/login").failureUrl("/login-error");	
-	}
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
-				.withUser("user").password("password").roles("USER");
-	}
-}
-}
+//package com.codeknights.ProEstimates1.configuration;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurity extends WebSecurityConfigurerAdapter {
+//}
